d47d39b70980ca6a8ac882fe47158683e216285d
tfabre <mas.tho3@yahoo.fr>
2021-12-01 09:19:22 +0100

    Solve day 1 part 2

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,29 @@ fn solve_day_1p1(filename: &str) -> Result<u32> {
         .sum())
 }
 
+fn solve_day_1p2(filename: &str) -> Result<u32> {
+    let file = File::open(filename)?;
+    let buffer = BufReader::new(file);
+    let lines = buffer
+        .lines()
+        .map(|line| line.unwrap())
+        .map(|line| line.parse::<u32>().unwrap())
+        .collect::<Vec<u32>>();
+
+    let windows: Vec<u32> = lines
+        .windows(3)
+        .map(|elem| elem.into_iter().sum())
+        .collect();
+
+    Ok(windows
+        .iter()
+        .zip(windows.iter().skip(1))
+        .map(|(previous, next)| if previous < next { 1 } else { 0 })
+        .sum())
+}
+
 fn main() -> Result<()> {
     println!("day 1.1 {}", solve_day_1p1("resources/day1")?);
+    println!("day 1.2 {}", solve_day_1p2("resources/day1")?);
     Ok(())
 }
