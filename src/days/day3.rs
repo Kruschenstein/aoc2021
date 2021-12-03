e786a86003d05ed69b271ae939150294e7eda0d9
tfabre <mas.tho3@yahoo.fr>
2021-12-03 18:04:36 +0100

    Solve day 3 part 2

--- src/days/day3.rs
+++ src/days/day3.rs
@@ -37,3 +37,57 @@ fn to_gamma_and_epsilon(lines_len: usize, count: &[u32]) -> (u32, u32) {
 
     (bytes_to_num(&gamma), bytes_to_num(&epsilon))
 }
+
+pub fn solve_p2(filename: &str) -> Result<u32> {
+    let lines = read_file_lines(filename)?;
+    let line_len = lines[0].len();
+    let mut oxygen = lines;
+    let mut co2 = oxygen.clone();
+
+    for i in 0..line_len {
+        let count1_oxygen = get_number_of_1_at_position(&oxygen, i);
+        let count1_co2 = get_number_of_1_at_position(&co2, i);
+        let oxygen_len = oxygen.len() as u32;
+        let co2_len = co2.len() as u32;
+        let keep1_oxygen = count1_oxygen >= (oxygen_len - count1_oxygen);
+        let keep1_co2 = count1_co2 < (co2_len - count1_co2);
+
+        if oxygen_len > 1 {
+            oxygen = oxygen
+                .into_iter()
+                .filter(|elem| {
+                    let c = elem.chars().nth(i).unwrap();
+                    keep1_oxygen && c == '1' || !keep1_oxygen && c == '0'
+                })
+                .collect();
+        }
+        if co2_len > 1 {
+            co2 = co2
+                .into_iter()
+                .filter(|elem| {
+                    let c = elem.chars().nth(i).unwrap();
+                    keep1_co2 && c == '1' || !keep1_co2 && c == '0'
+                })
+                .collect();
+        }
+    }
+
+    let oxygen_num = oxygen[0]
+        .chars()
+        .map(|e| if e == '1' { 1 } else { 0 })
+        .collect::<Vec<_>>();
+    let co2_num = co2[0]
+        .chars()
+        .map(|e| if e == '1' { 1 } else { 0 })
+        .collect::<Vec<_>>();
+
+    Ok(bytes_to_num(&oxygen_num) * bytes_to_num(&co2_num))
+}
+
+fn get_number_of_1_at_position(lines: &[String], position: usize) -> u32 {
+    lines
+        .iter()
+        .map(|line| line.chars().nth(position).unwrap())
+        .map(|e| if e == '1' { 1 } else { 0 })
+        .sum()
+}
